5a022ec69e2d1e8b953a18906732f78a26d14aa9
Matthias H <apoc@sixserv.org>
2013-09-23 22:12:46 +0200

    add a share clipboard "button" to the preferences
    also renamed the preferences activity and licensed
    it GPLv3. The new share feature is implemented as a
    "preference" somewhat ugly hack, but quickest solution :P

---
+++ src/org/geekosphere/sharewithwoot/SharePreferences.java
@@ -0,0 +1,55 @@
+package org.geekosphere.sharewithwoot;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+
+public class SharePreferences extends PreferenceActivity implements OnPreferenceClickListener {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        Preference shareFromClipboard = findPreference("KEY_PASTE");
+        shareFromClipboard.setOnPreferenceClickListener(this);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onPreferenceClick(Preference p) {
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
+
+            String url = item.getText().toString();
+            if (url.startsWith("http")) {
+                shareIntent(url);
+            }
+        }
+        else {
+            android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+            String url = clipboard.getText().toString();
+            if (url.startsWith("http")) {
+                shareIntent(url);
+            }
+        }
+
+        return false;
+    }
+
+    private void shareIntent(String url) {
+        Intent sendIntent = new Intent(this, ShareActivity.class);
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, url);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+}
